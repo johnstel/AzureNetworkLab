2506d0e8e1d771a683a1101380cd667094767ba8
John Stelmaszek <johnstel@users.noreply.github.com>
2018-12-10 12:00:42 -0500

    Update DemoAppConfig.sh

--- DemoAppConfig.sh
+++ DemoAppConfig.sh
@@ -1,14 +1,16 @@
+
 # Update system
 sudo apt-get update
 
 # Install node.js & NPM
 sudo apt-get install npm -y
-sudo apt-get install nodejs-legacy -y
+curl -sL https://deb.nodesource.com/setup_9.x | sudo -E bash --
+sudo apt-get install -y nodejs
+
 
 # Clone Git repo for demo app
 sudo apt-get install git -y
-# git clone https://github.com/araffe/nodejs-demoapp.git
-git clone https://github.com/johnstel/nodejs-demoapp.git
+git clone https://github.com/araffe/nodejs-demoapp.git
 cd nodejs-demoapp/
 sudo npm install forever -g
 sudo npm install
